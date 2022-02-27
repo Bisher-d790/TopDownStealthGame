ca95eb65ba4cea6867ca5a6b20d627c1f4d72fd8
Bisher Dokkmak <35769802+Bisher-d790@users.noreply.github.com>
2022-02-27 16:45:50 +0800

    [Upgrade] Setup basic project in UE5.

--- Source/EnemyGuards/EnemyGuards.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "EnemyGuards.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EnemyGuards, "EnemyGuards" );
-
-DEFINE_LOG_CATEGORY(LogEnemyGuards)
- 
