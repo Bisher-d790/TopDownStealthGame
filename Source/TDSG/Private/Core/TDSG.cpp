d35fee574c0bfd853c6997f44ba88d5f2854f806
Bisher Dokkmak <35769802+Bisher-d790@users.noreply.github.com>
2022-02-27 21:31:21 +0800

    [Setup] Setup basic enemy character.

---
+++ Source/TDSG/Private/Core/TDSG.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Core/TDSG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TDSG, "TDSG" );
+
+DEFINE_LOG_CATEGORY(LogTDSG)
+ 
